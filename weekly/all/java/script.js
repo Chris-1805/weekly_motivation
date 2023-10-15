79e0761fac35a99359e8c515061e4c60ebd7e9da
Chris-1805 <147304645+Chris-1805@users.noreply.github.com>
2023-10-15 18:53:43 +0200

    Add files via upload

---
+++ weekly/all/java/script.js
@@ -0,0 +1,41 @@
+// Erhalte das Header-Element mit der Klasse "headerandnav"
+var header = document.querySelector(".headerandnav");
+
+// Erhalte alle Sektionen mit der Klasse "untertitel"
+var subtitle = document.querySelectorAll(".untertitel");
+
+// Füge ein Scroll-Ereignis hinzu
+window.addEventListener("scroll", function() {
+    // Überprüfe, ob der Header gescrollt ist und füge die Klasse "scrolled" hinzu
+    if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+
+    // Überprüfe jede Sektion
+    subtitle.forEach(function(subtitle) {
+        // Überprüfe, ob die untere Kante des Header-Bereichs den oberen Rand der Sektion berührt
+        if (header.getBoundingClientRect().bottom >= subtitle.getBoundingClientRect().top) {
+            // Füge die CSS-Klasse "transparent" hinzu, um die Schriftfarbe zu ändern
+            subtitle.classList.add("transparent");
+        } else {
+            // Entferne die CSS-Klasse, wenn nicht berührt
+            subtitle.classList.remove("transparent");
+        }
+    });
+});
+
+
+function scrollToSubtitle(subtitleId) {
+    const headerHeight = document.getElementById("headerandnav").offsetHeight;
+    const subtitleElement = document.getElementByClass("untertitel");
+
+    if (subtitleElement) {
+        const subtitlePosition = subtitleElement.getBoundingClientRect().top + window.scrollY;
+        window.scrollTo({
+            top: screenTop - headerHeight,
+            behavior: 'smooth'
+        });
+    }
+}
