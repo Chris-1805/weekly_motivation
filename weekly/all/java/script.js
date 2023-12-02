09cf2b6218a81e6d9cfa4853db66d079a0cd01d8
Chris-1805 <ch.schmidt@stud.unibas.ch>
2023-12-02 22:15:11 +0100

    .

--- weekly/all/java/script.js
+++ weekly/all/java/script.js
@@ -1,47 +1,47 @@
-// Erhalte das Header-Element mit der Klasse "headerandnav"
-var header = document.querySelector(".headerandnav");
-// Erhalte das Header-Element mit der ID "headerandnav" statt der Klasse "headerandnav"
-var header = document.getElementById("headerandnav");
+// script.js
 
-
-
-function scrollToSubtitle(subtitleId) {
-    const headerHeight = header.offsetHeight; // Use header variable
-    const subtitleElement = document.querySelector(subtitleId); // Correct typo
-    
-    if (subtitleElement) {
-        const subtitlePosition = subtitleElement.getBoundingClientRect().top + window.scrollY;
-        window.scrollTo({
-            top: subtitlePosition - headerHeight, // Use the correct variables
-            behavior: 'auto'
-        });
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.headerandnav');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
     }
-}
-
-// Erhalte alle Sektionen mit der Klasse "untertitel"
-var subtitle = document.querySelectorAll(".untertitel");
-
-// Füge ein Scroll-Ereignis hinzu
-window.addEventListener("scroll", function() {
-    // Überprüfe, ob der Header gescrollt ist und füge die Klasse "scrolled" hinzu
-      if (header !== null && window.scrollY > 0) {
-        header.classList.add("scrolled");
-      } else if (header !== null) {
-        header.classList.remove("scrolled");
-      }
-    // Überprüfe jede Sektion
-    subtitle.forEach(function(subtitle) {
-        // Überprüfe, ob die untere Kante des Header-Bereichs den oberen Rand der Sektion berührt
-        if (header.getBoundingClientRect().bottom >= subtitle.getBoundingClientRect().top) {
-            // Füge die CSS-Klasse "transparent" hinzu, um die Schriftfarbe zu ändern
-            subtitle.classList.add("transparent");
-        } else {
-            // Entferne die CSS-Klasse, wenn nicht berührt
-            subtitle.classList.remove("transparent");
-        }
-    });
 });
 
+window.addEventListener('scroll', function() {
+    var scrollToTopButton = document.getElementById('totop');
+    if (window.pageYOffset > 0) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+document.getElementById('scrollToTopButton').addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                const offsetTop = targetElement.getBoundingClientRect().top + window.scrollY;
+                window.scrollTo({
+                    top: offsetTop - document.querySelector('.headerandnav').offsetHeight,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
 
 
 
